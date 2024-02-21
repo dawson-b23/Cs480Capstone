9b66bb927f105b16575c5d86cf8ecb2f57b61e6b
shreckneps <shreckneps@gmail.com>
2024-02-21 14:50:35 -0800

    Comparing positions of the two modules.

--- source/basicAlignment/src/main.cpp
+++ source/basicAlignment/src/main.cpp
@@ -20,46 +20,91 @@ void setup() {
   phase = 0;
 }
 
+double rbtLat;
+double rbtLng;
+double lsrLat;
+double lsrLng;
+
+int included = 0;
+double deltaLat = 0;
+double deltaLng = 0;
+
 void loop() {
   static char charRead;
   if(phase == 0) {
-    Serial.println("Phase 0");
+    //Serial.println("Phase 0");
     while(laserSerial.available()) {
       charRead = laserSerial.read();
+      //Serial.print(charRead);
       laserGPS.encode(charRead);
     }
-    if(laserGPS.location.isValid()) {
+    if(laserGPS.location.isUpdated()) {
       phase++;
       laserSerial.end();
       robotSerial.begin(9600);
-      Serial.println("Laser location obtained: ");
+      Serial.print("Laser location obtained, HDOP ");
+      Serial.print(laserGPS.hdop.hdop(), 20);
+      Serial.println(": ");
+      lsrLat = laserGPS.location.lat();
+      lsrLng = laserGPS.location.lng();
       Serial.print("Lat: ");
-      Serial.println(laserGPS.location.lat());
+      Serial.println(lsrLat, 20);
       Serial.print("Lng: ");
-      Serial.println(laserGPS.location.lng());
+      Serial.println(lsrLng, 20);
     }
-    delay(1500);
+    delay(1);
   }
 
   if(phase == 1) {
-    Serial.println("Phase 1");
+    //Serial.println("Phase 1");
     while(robotSerial.available()) {
       charRead = robotSerial.read();
+      //Serial.print(charRead);
       robotGPS.encode(charRead);
     }
-    if(robotGPS.location.isValid()) {
-      phase++;
+    if(robotGPS.location.isUpdated()) {
+      phase = 0;
       robotSerial.end();
-      Serial.println("Robot location obtained: ");
+      laserSerial.begin(9600);
+      Serial.print("Robot location obtained, HDOP ");
+      Serial.print(robotGPS.hdop.hdop(), 20);
+      Serial.println(": ");
+      rbtLat = robotGPS.location.lat();
+      rbtLng = robotGPS.location.lng();
       Serial.print("Lat: ");
-      Serial.println(robotGPS.location.lat());
+      Serial.println(rbtLat, 20);
       Serial.print("Lng: ");
-      Serial.println(robotGPS.location.lng());
+      Serial.println(rbtLng, 20);
+
+      Serial.print("Distance between modules: ");
+      Serial.println(robotGPS.distanceBetween(lsrLat, lsrLng, rbtLat, rbtLng), 30);
+
+      if(included) {
+        included++;
+        deltaLat = deltaLat * ((included - 1) / (double) included);
+        deltaLat += (rbtLat - lsrLat) * (1.0 / included);
+        deltaLng = deltaLng * ((included - 1) / (double) included);
+        deltaLng += (rbtLng - lsrLng) * (1.0 / included);
+      } else {
+        included++;
+        deltaLat = rbtLat - lsrLat;
+        deltaLng = rbtLng - lsrLng;
+      }
+      Serial.print("From ");
+      Serial.print(included);
+      Serial.println(" samples, average displacement is: ");
+      Serial.print("Lat: ");
+      Serial.println(deltaLat, 20);
+      Serial.print("Lng: ");
+      Serial.println(deltaLng, 20);
+
+      Serial.println();
     }
-    delay(1500);
+    delay(1);
   }
 
   if(phase == 2) {
+    return;
     Serial.println("Phase 2");
     arm.setPosition(5, 0, 1500, true);
     delay(2500);
